0b35b4a215e091e543a82e15a66239e447317da9
Nikita Podoliako <00nktk@hey.com>
2023-02-26 00:58:01 +0700

    refactor: factor out volca connection logic

--- src/main.rs
+++ src/main.rs
@@ -1,286 +1,43 @@
 mod audio;
+mod device;
 mod opt;
 mod proto;
 mod seven_bit;
 mod util;
 
-use std::any::type_name;
 use std::borrow::Cow;
-use std::ffi::CString;
-use std::fmt::Debug;
 use std::io;
 use std::path::{Path, PathBuf};
-use std::time::Duration;
 
-use alsa::seq::{self, ClientInfo};
 use anyhow::{anyhow, bail, Result};
 use clap::Parser;
-use proto::Header;
-use seven_bit::U7;
-use smallvec::SmallVec;
-use tracing::{debug, info, trace, warn};
+use tracing::warn;
 
 use crate::audio::{write_sample_to_file, AudioReader, MonoMode};
-use crate::util::{hexbuf, DEBUG_TRESHOLD};
-
-const SELF_NAME: &str = "VolSa2";
-
-fn find_volca(seq: &seq::Seq) -> Result<seq::Addr> {
-    let mut clients = seq::ClientIter::new(seq);
-
-    let client: ClientInfo = clients
-        .find(|client| {
-            trace!(?client, "trying client");
-            client
-                .get_name()
-                .ok()
-                .filter(|&name| name == "volca sample")
-                .is_some()
-        })
-        .ok_or_else(|| anyhow!("could not find volca sample"))?;
-
-    let port = seq::PortIter::new(seq, client.get_client())
-        .next()
-        .ok_or_else(|| anyhow!("no port"))?;
-
-    Ok(port.addr())
-}
-
-struct State {
-    seq: seq::Seq,
-    me: seq::Addr,
-    volca: seq::Addr,
-    channel: U7,
-    chunk_cooldown: Duration,
-}
-
-impl State {
-    fn new(chunk_cooldown: Duration) -> Result<Self> {
-        let seq = seq::Seq::open(None, None, false)?;
-        seq.set_client_name(&CString::new(SELF_NAME)?)?;
-        let mut me = seq::PortInfo::empty()?;
-        me.set_capability(
-            seq::PortCap::WRITE
-            | seq::PortCap::SUBS_WRITE
-            | seq::PortCap::READ
-            | seq::PortCap::SUBS_READ
-            // | seq::PortCap::SYNC_READ
-            // | seq::PortCap::SYNC_WRITE
-            | seq::PortCap::DUPLEX,
-        );
-        me.set_type(seq::PortType::MIDI_GENERIC | seq::PortType::APPLICATION | seq::PortType::PORT);
-        me.set_name(&CString::new(SELF_NAME)?);
-
-        seq.create_port(&me)?;
-
-        let volca = find_volca(&seq)?;
-        let me = me.addr();
-
-        Ok(Self {
-            me,
-            seq,
-            volca,
-            channel: U7::new(0),
-            chunk_cooldown,
-        })
-    }
-
-    fn connect(&mut self) -> Result<()> {
-        let sub = seq::PortSubscribe::empty()?;
-        sub.set_sender(self.volca);
-        sub.set_dest(self.me);
-        self.seq.subscribe_port(&sub)?;
-
-        let sub = seq::PortSubscribe::empty()?;
-        sub.set_sender(self.me);
-        sub.set_dest(self.volca);
-        self.seq.subscribe_port(&sub)?;
-
-        let echo = U7::new(42);
-        self.send(proto::SearchDeviceRequest { echo })?;
-
-        let (_, response) = self.receive::<proto::SearchDeviceReply>()?;
-        info!(
-            global_channel = %response.device_id, version = %response.version,
-            "connected to volca sample 2"
-        );
-        self.channel = response.device_id;
-        Ok(())
-    }
-
-    fn send<T>(&self, msg: T) -> Result<()>
-    where
-        T: proto::Outgoing + Debug,
-        T::Header: Debug,
-    {
-        let mut buf = SmallVec::<[u8; 6]>::new();
-        let header = T::Header::from_channel(self.channel);
-        msg.encode(header, &mut buf)?;
-
-        if buf.len() > DEBUG_TRESHOLD {
-            debug!(msg = type_name::<T>(), len = buf.len(), "send msg");
-            trace!(?msg, raw = ?hexbuf(&buf), len = buf.len(), "send msg");
-        } else {
-            debug!(?msg, len = buf.len(), "send msg");
-        }
-
-        for slice in buf.chunks(256) {
-            let mut event = seq::Event::new_ext(seq::EventType::Sysex, slice);
-
-            trace!(len = slice.len(), raw = ?hexbuf(slice), "send chunk");
-
-            event.set_source(self.me.port);
-            event.set_direct();
-            event.set_priority(true);
-            event.set_dest(self.volca);
-
-            self.seq.event_output_direct(&mut event)?;
-            if !slice.ends_with(&[proto::EOX]) && !self.chunk_cooldown.is_zero() {
-                std::thread::sleep(self.chunk_cooldown);
-            }
-        }
-        self.seq.sync_output_queue()?;
-        self.seq.drain_output()?;
-
-        Ok(())
-    }
-
-    fn receive<T>(&self) -> Result<(T::Header, T)>
-    where
-        T: proto::Incoming + Debug,
-        T::Header: Debug,
-    {
-        self.seq.set_client_pool_input(1024)?;
-        let mut input = self.seq.input();
-
-        macro_rules! next_event {
-            () => {
-                loop {
-                    let event = input.event_input()?;
-                    if event.get_type() == seq::EventType::Sysex
-                        && event.get_source() == self.volca
-                        && event.get_dest() == self.me
-                    {
-                        break event;
-                    }
-                }
-            };
-        }
-
-        let event = next_event!();
-        let mut owned_data = None;
-        let mut data = event
-            .get_ext()
-            .ok_or_else(|| anyhow!("SysEx without data"))?;
-        trace!(raw = ?hexbuf(data), len = data.len(), "recv fst chunk");
-
-        #[allow(unused_assignments)]
-        // TODO: Fix this
-        if !data.ends_with(&[proto::EOX]) {
-            owned_data.replace(data.to_vec());
-            data = &[]; // Free input borrow
-
-            while !owned_data
-                .as_ref()
-                .expect("replaced")
-                .ends_with(&[proto::EOX])
-            {
-                let event = next_event!();
-                let new_data = event
-                    .get_ext()
-                    .ok_or_else(|| anyhow!("SysEx without data"))?;
-                trace!(raw = ?hexbuf(new_data), len = new_data.len(), "recv chunk");
-                owned_data
-                    .as_mut()
-                    .expect("replaced earlier")
-                    .extend(new_data);
-            }
-            data = owned_data.as_ref().expect("replaced");
-        }
-
-        let data = &data;
-        let msg = T::parse(data).map_err(Into::into);
-        if data.len() > DEBUG_TRESHOLD {
-            debug!(msg = type_name::<T>(), len = data.len(), "recv msg");
-            trace!(?msg, raw = ?hexbuf(data), "recv_msg");
-        } else {
-            debug!(?msg, raw = ?hexbuf(data), len = data.len(), "recv_msg");
-        }
-        msg
-    }
-
-    fn list_sample_headers(&self) -> impl Iterator<Item = Result<proto::SampleHeader>> + '_ {
-        (0..200).map(|idx| {
-            self.send(proto::SampleHeaderDumpRequest { sample_no: idx })?;
-            let (_, response) = self.receive::<proto::SampleHeader>()?;
-            Ok(response)
-        })
-    }
-
-    fn get_sample_header(&self, sample_no: u8) -> Result<proto::SampleHeader> {
-        // TODO: restrict this in type
-        if sample_no > 199 {
-            bail!("sample_no must be less than 200");
-        }
-
-        self.send(proto::SampleHeaderDumpRequest { sample_no })?;
-        let (_, header) = self.receive::<proto::SampleHeader>()?;
-        Ok(header)
-    }
-
-    fn get_sample(&self, sample_no: u8) -> Result<proto::SampleData> {
-        // TODO: restrict this in type
-        if sample_no > 199 {
-            bail!("sample_no must be less than 200");
-        }
-
-        self.send(proto::SampleDataDumpRequest { sample_no })?;
-        let (_, sample_data) = self.receive::<proto::SampleData>()?;
-        Ok(sample_data)
-    }
-
-    fn delete_sample(&self, sample_no: u8) -> Result<()> {
-        // TODO: restrict this in type
-        if sample_no > 199 {
-            bail!("sample_no must be less than 200");
-        }
-
-        self.send(proto::SampleHeader::empty(sample_no))?;
-        self.receive::<proto::Status>()?.1?;
-        Ok(())
-    }
-
-    fn send_sample(&self, header: proto::SampleHeader, data: proto::SampleData) -> Result<()> {
-        self.send(header)?;
-        self.receive::<proto::Status>()?.1?;
-        self.send(data)?;
-        self.receive::<proto::Status>()?.1?;
-        Ok(())
-    }
-}
+use crate::device::Device;
 
 fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
     let opts = opt::Opts::parse();
 
-    let init_state = || -> Result<State> {
-        let mut state = State::new(opts.chunk_cooldown.into())?;
-        state.connect()?;
-        Ok(state)
+    let init_volca = || -> Result<Device> {
+        let mut volca = Device::new(opts.chunk_cooldown.into())?;
+        volca.connect()?;
+        Ok(volca)
     };
 
     match opts.cmd {
         opt::Operation::List { show_empty } => {
-            let state = init_state()?;
+            let volca = init_volca()?;
 
-            state.send(proto::SampleSpaceDumpRequest)?;
-            let (_, response) = state.receive::<proto::SampleSpaceDump>()?;
+            volca.send(proto::SampleSpaceDumpRequest)?;
+            let (_, response) = volca.receive::<proto::SampleSpaceDump>()?;
             println!("Occupied space: {:.1}%", response.occupied() * 100.);
 
             let mut last_printed = 0;
-            for header in state
-                .list_sample_headers()
+            for header in volca
+                .iter_sample_headers()
                 .filter(|res| res.as_ref().map_or(true, |header| !header.is_empty()))
             {
                 let header = header?;
@@ -297,11 +54,11 @@ fn main() -> Result<()> {
             }
         }
         opt::Operation::Download { sample_no, output } => {
-            let state = init_state()?;
+            let volca = init_volca()?;
 
-            let header = state.get_sample_header(sample_no)?;
+            let header = volca.get_sample_header(sample_no)?;
             println!(r#"Downloading sample "{}" from Volca"#, header.name);
-            let sample_data = state.get_sample(sample_no)?;
+            let sample_data = volca.get_sample(sample_no)?;
 
             let output = normalize_path(&output, &header.name);
             write_sample_to_file(&sample_data.data, &output)?;
@@ -335,11 +92,11 @@ fn main() -> Result<()> {
                 return Ok(());
             }
 
-            let state = init_state()?;
+            let volca = init_volca()?;
             let sample_no = sample_no
                 .map(Ok)
                 .or_else(|| {
-                    state.list_sample_headers().find_map(|result| {
+                    volca.iter_sample_headers().find_map(|result| {
                         result
                             .map(|header| header.is_empty().then_some(header.sample_no))
                             .transpose()
@@ -347,7 +104,7 @@ fn main() -> Result<()> {
                 })
                 .ok_or_else(|| anyhow!("could not find empty slot"))??;
 
-            let current_header = state.get_sample_header(sample_no)?;
+            let current_header = volca.get_sample_header(sample_no)?;
             if !current_header.is_empty() {
                 // TODO: format_args?
                 let question = format!(
@@ -362,7 +119,7 @@ fn main() -> Result<()> {
                     "Do you want to backup the loaded sample ({})?",
                     current_header.name
                 ))? {
-                    let sample = state.get_sample(sample_no)?;
+                    let sample = volca.get_sample(sample_no)?;
                     let path = format!("./{}.wav", current_header.name);
                     write_sample_to_file(&sample.data, path.as_ref())?;
                     println!("Wrote backup to {path}");
@@ -370,16 +127,16 @@ fn main() -> Result<()> {
             }
 
             let (header, data) = proto::SampleData::new(sample_no, &name, sample);
-            state.send_sample(header, data)?;
+            volca.send_sample(header, data)?;
             println!("Loaded sample {name} in slot {sample_no}");
         }
         opt::Operation::Remove {
             sample_no,
             print_name,
         } => {
-            let state = init_state()?;
+            let volca = init_volca()?;
             let name = if print_name {
-                let mut header = state.get_sample_header(sample_no)?;
+                let mut header = volca.get_sample_header(sample_no)?;
                 if header.is_empty() {
                     println!("Sample is already empty");
                     return Ok(());
@@ -391,7 +148,7 @@ fn main() -> Result<()> {
                 String::new()
             };
 
-            state.delete_sample(sample_no)?;
+            volca.delete_sample(sample_no)?;
             println!("Removed sample {name}at slot {sample_no}");
         }
     }
